e84b055298f8103bfdea918a0867a8a3c6af99e8
Illia Tkachuk <tkachuk.i.m@gmail.com>
2018-10-11 23:22:07 +0200

    Implemented sending email on found reservations

---
+++ urzad_gmail_sender.py
@@ -0,0 +1,32 @@
+import configparser
+import smtplib
+from email.mime.text import MIMEText
+
+user_config = configparser.ConfigParser()
+user_config.read('data/user.ini')
+
+gmail_user = user_config['gmail']['email']
+gmail_password = user_config['gmail']['password']
+
+
+def send_mail(url):
+    sent_from = gmail_user
+    to = ['***REMOVED***']
+    subject = 'Urzad Bot: I was able to lock a slot for you. Hurry up!!!'
+    body = f'Here is the URL for you my master: {url}'
+
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = sent_from
+    msg['To'] = ", ".join(to)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.send_message(msg)
+        server.close()
+
+        print('Email sent!')
+    except Exception as e:
+        print('Something went wrong...' + str(e))
--- urzad_lock.py
+++ urzad_lock.py
@@ -10,14 +10,23 @@ import urllib3
 import AdvancedHTMLParser
 
 from urzad_common import *
+from urzad_gmail_sender import send_mail
 
 logger = logging.getLogger('urzadDate')
+
+global_locked_slots = []
 
 def get_available_slots(response, date):
     # print(response.text)
     parser = AdvancedHTMLParser.AdvancedHTMLParser()
     parser.parseStr(response.text)
     slots_found = [date + ' ' + a.text + ':00' for a in parser.getElementsByTagName('a') if a.id != "confirmLink"]
+    # for testing purposes: 
+    # slots_found = [date + ' ' + a + ':00' for a in [
+    #     '10:00', '10:20', '10:40', 
+    #     '11:00', '11:20', '11:40'
+    #     '12:00', '12:20', '12:40',
+    #     '12:00', '13:20', '13:40']]
     # print(slots_found)
     return slots_found
 
@@ -45,7 +54,16 @@ def lock_slot(session, loc, city, date, time):
     logger.debug(f"Going to lock slot {time} for {loc}: {city}...")
     response = session.post(page_lock, cookies={'config[currentLoc]':loc,'AKIS':session.cookies['AKIS']}, headers={'X-Requested-With':'XMLHttpRequest'}, verify=False, data={'time': time, 'queue': queue})
     logger.debug(f"Lock response for slot {time} for {loc}: {city} is === {response.text} ===")
-    logger.info(response.text)
+    if "OK " in response.text:
+        slot_id = response.text[3:]
+        if slot_id not in global_locked_slots:
+            logger.info(f'A slot found: {slot_id} Sending a email with URL...')
+            global_locked_slots.append(slot_id)
+            url = page_form + slot_id + '/1'
+            send_mail(url)
+        else:
+            logger.info(f'A slot found: {slot_id} and was already locked by me. Check email!!!')
+
 
 def get_slots(session, loc, city, page):
     date = dates_config['loc_'+loc]['date']
